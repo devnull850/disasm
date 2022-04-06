d7f5a93d6618e8cc983124708ceb5869d4d75fbb
devnull850 <dev.null850@gmail.com>
2022-04-06 08:29:24 -0400

    Working implementation for snaked ELF tiny bins

--- src/disasm.c
+++ src/disasm.c
@@ -15,8 +15,7 @@ static csh open_capstone_handle(void) {
 }
 
 static int is_cs_cflow_group(uint8_t g) {
-	return g == CS_GRP_JUMP || g == CS_GRP_CALL ||
-		g == CS_GRP_RET || g == CS_GRP_IRET;
+	return g == CS_GRP_JUMP || g == CS_GRP_CALL || g == CS_GRP_RET || g == CS_GRP_IRET;
 }
 
 static int is_cs_cflow_ins(cs_insn *ins) {
@@ -29,22 +28,36 @@ static int is_cs_cflow_ins(cs_insn *ins) {
 	return 0;
 }
 
+static int is_cs_unconditional_cflow_ins(cs_insn *ins) {
+	switch (ins->id) {
+		case X86_INS_JMP:
+		case X86_INS_LJMP:
+		case X86_INS_RET:
+		case X86_INS_RETF:
+		case X86_INS_RETFQ:
+			return 1;
+
+		default:
+			return 0;
+	}
+}
+
 static void linear_disassemble(struct Elf64_bin *elf) {
-    csh handle;
+	csh handle;
 	cs_insn *insn;
 	size_t count;
 
-    handle = open_capstone_handle();
+	handle = open_capstone_handle();
 
 	for (size_t i = 0; i < elf->ehdr->e_phnum; ++i) {
 		if (!(elf->phdr[i].p_flags & 1)) continue;
 
 		count = cs_disasm(handle, elf->segments[i].bytes, elf->phdr[i].p_filesz,
-				elf->phdr[i].p_vaddr, 0, &insn);
+						  elf->phdr[i].p_vaddr, 0, &insn);
 
 		for (size_t j = 0; j < count; ++j) {
 			printf("0x%0lx:\t%s\t\t%s\n",
-				insn[j].address, insn[j].mnemonic, insn[j].op_str);
+				   insn[j].address, insn[j].mnemonic, insn[j].op_str);
 		}
 
 		cs_free(insn, count);
@@ -58,62 +71,84 @@ static void recursive_disassemble(struct Elf64_bin *elf) {
 	cs_insn *insn;
 	size_t count, code_size;
 	int flag;
-	uint8_t *p, *p1, b;
-	struct AddressList *list;
+	uint8_t *p, b;
+	struct AddressList *visited, *addr_queue;
 	uint64_t target;
 
-    handle = open_capstone_handle();
+	handle = open_capstone_handle();
 	cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON);
-
-	list = new_address_list();
 
 	for (size_t i = 0; i < elf->ehdr->e_phnum; ++i) {
 		if (!(elf->phdr[i].p_flags & 1)) continue;
 
 		p = elf->segments[i].bytes + elf->ehdr->e_entry;
-		p1 = elf->segments[i].bytes;
 		code_size = elf->segments[i].size - elf->ehdr->e_entry;
+		target = elf->ehdr->e_entry;
+
+		visited = new_address_list(elf->segments[i].size);
+		addr_queue = new_address_list(elf->segments[i].size);
 
 		flag = 1;
 
 		while (flag) {
-			count = cs_disasm(handle, p, code_size, 0, 0, &insn);
-			if (!count) break;
+			if (!(count = cs_disasm(handle, p, code_size, 0, 0, &insn))) break;
 
 			for (size_t j = 0; j < count; ++j) {
 				printf("0x%0lx:\t%s\t\t%s\n",
-					insn[j].address, insn[j].mnemonic, insn[j].op_str);
+					   target + insn[j].address, insn[j].mnemonic, insn[j].op_str);
 
-				if (is_cs_cflow_ins(insn+j)) {
+				if (!contains(visited, target + insn[j].address)) {
+					add_address(visited, target + insn[j].address);
+				}
+
+				if (is_cs_cflow_ins(insn + j)) {
 					b = insn[j].bytes[1];
 
-					if (((b>>7) & 1)) {
+					if (!is_cs_unconditional_cflow_ins(insn + j)) {
+						if (!contains(addr_queue, target + insn[j].address + insn[j].size)) {
+							add_address(addr_queue, target + insn[j].address + insn[j].size);
+						}
+					}
+
+					if (((b >> 7) & 1)) {
 						b = 0 - b;
-						target = (p - p1) + insn[j].address + insn[j].size;
+						target = (p - elf->segments[i].bytes) + insn[j].address + insn[j].size;
 						target -= b;
 					}
 					else {
-						target = (p - p1) + b + insn[j].address + insn[j].size;
+						target = (p - elf->segments[i].bytes) + b + insn[j].address + insn[j].size;
 					}
 
-					if (contains(list, target)) {
-						flag = 0;
+					if (contains(visited, target)) {
+						if (is_empty(addr_queue)) {
+							flag = 0;
+						}
+						else {
+							target = remove_address(addr_queue);
+							add_address(visited, target);
+							p = elf->segments[i].bytes + target;
+							code_size = elf->segments[i].size - (p - elf->segments[i].bytes);
+						}
+
 						break;
 					}
 
-					add_address(list, target);
-					p = p1 + target;
-					code_size = (p1 + target) - elf->segments[i].bytes;
+					p = elf->segments[i].bytes + target;
+					code_size = elf->segments[i].size - (p - elf->segments[i].bytes);
 
 					break;
 				}
+
+				if (insn[j].address + insn[j].size == code_size) flag = 0;
 			}
 
 			cs_free(insn, count);
 		}
+
+		free_address_list(visited);
+		free_address_list(addr_queue);
 	}
 
-	free_address_list(list);
 	cs_close(&handle);
 }
 
@@ -122,9 +157,11 @@ void disassemble(struct Elf64_bin *elf, enum Disassembly type) {
 		case LINEAR:
 			linear_disassemble(elf);
 			break;
+
 		case RECURSIVE:
 			recursive_disassemble(elf);
 			break;
+
 		default:
 			handle_error("invalid disassembly type");
 	}
